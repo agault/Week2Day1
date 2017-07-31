e971182b402c279eaf1838de48903f0fe45e3b75
Asia Gault <asialevesquegault@gmail.com>
2017-07-31 16:19:01 +0000

    week2day1

---
+++ step1.js
@@ -0,0 +1,26 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (chunk) {
+      console.log(`${chunk}${'\n'}`);
+    });
+
+    // response.on('end', function () {
+    //   console.log('Response stream complete.');
+    // });
+    // console.log(getAndPrintHTMLChunks(data + '\n'))
+  });
+};
+
+getAndPrintHTMLChunks()
